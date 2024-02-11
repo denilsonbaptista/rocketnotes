428aa61a0aa2b73dab427d35900b5880d6f9eb1f
denilsonbaptista <baptistadenilsonbp@gmail.com>
2024-02-11 14:11:23 -0300

    authentication Hook

---
+++ src/hooks/auth.jsx
@@ -0,0 +1,21 @@
+import { createContext, useContext } from 'react';
+
+const AuthContext = createContext({});
+
+function AuthProvider({ children }) {
+  return (
+    <AuthContext.Provider
+      value={{ name: 'Denilson', email: 'denilson@email.com' }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+function useAuth() {
+  const context = useContext(AuthContext);
+
+  return context;
+}
+
+export { AuthProvider, useAuth };
