8afe005438aff85f6a9a55d56f3a14de17f916ae
Denilson Baptista <baptistadenilsonbp@gmail.com>
2024-02-15 10:58:44 -0300

    Add tag

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -14,6 +14,9 @@ export function New() {
   const [links, setLinks] = useState([]);
   const [newLink, setNewLink] = useState('');
 
+  const [tags, setTags] = useState([]);
+  const [newTag, setNewTag] = useState('');
+
   function handleAddLink() {
     setLinks(prevState => [...prevState, newLink]);
     setNewLink('');
@@ -21,6 +24,11 @@ export function New() {
 
   function handleRemoveLink(deleted) {
     setLinks(prevState => prevState.filter(link => link !== deleted));
+  }
+
+  function handleAddTag() {
+    setTags(prevState => [...prevState, newTag]);
+    setNewTag('');
   }
 
   return (
@@ -56,8 +64,17 @@ export function New() {
 
           <Section title="Marcadores">
             <div className="tags">
-              <NoteItem values="react" />
-              <NoteItem isNew placeholder="Nova tag" />
+              {tags.map((tag, index) => (
+                <NoteItem key={String(index)} values={tag} onClick={() => {}} />
+              ))}
+
+              <NoteItem
+                isNew
+                placeholder="Nova tag"
+                onChange={e => setNewTag(e.target.value)}
+                values={newTag}
+                onClick={handleAddTag}
+              />
             </div>
           </Section>
 
