29f894b7bbe0a547852540bd93b218ee664dbf9f
denilsonbaptista <baptistadenilsonbp@gmail.com>
2024-02-18 11:04:59 -0300

    treating routes unavailable

--- src/routes/auth.routes.jsx
+++ src/routes/auth.routes.jsx
@@ -1,13 +1,17 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 import { SingIn } from '../pages/SingIn';
 import { SingUp } from '../pages/SingUp';
 
 export function AuthRoutes() {
+  const user = localStorage.getItem('@rocketnotes:user');
+
   return (
     <Routes>
       <Route path="/" element={<SingIn />} />
       <Route path="/register" element={<SingUp />} />
+
+      {!user && <Route path="*" element={<Navigate to="/" />} />}
     </Routes>
   );
 }
